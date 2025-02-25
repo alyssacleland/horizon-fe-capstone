39647745299a7f97637719e3352f799fe49a3814
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-02-24 22:22:50 -0600

    create user obj on initial signin

--- src/utils/context/ViewDirector.js
+++ src/utils/context/ViewDirector.js
@@ -3,26 +3,30 @@ import { useAuth } from '@/utils/context/authContext';
 import Loading from '@/components/Loading';
 import SignIn from '@/components/SignIn';
 import NavBar from '@/components/NavBar';
+import useUser from '@/utils/hooks/useUser';
 
 function ViewDirectorBasedOnUserAuthStatus({ children }) {
   const { user, userLoading } = useAuth();
+  const { userData, loading } = useUser();
 
   // if user state is null, then show loader
-  if (userLoading) {
+  // Show loading spinner while user data is being fetched
+  if (userLoading || loading) {
     return <Loading />;
   }
 
-  // what the user should see if they are logged in
-  if (user) {
-    return (
-      <>
-        <NavBar /> {/* NavBar only visible if user is logged in and is in every view */}
-        {children}
-      </>
-    );
+  // Allow rendering SignIn if user is not authenticated or userData is not available
+  if (!user || !userData) {
+    return <SignIn />;
   }
 
-  return <SignIn />;
+  // what the user should see if they are logged in and data is available
+  return (
+    <>
+      <NavBar /> {/* NavBar only visible if user is logged in and is in every view */}
+      {children}
+    </>
+  );
 }
 
 export default ViewDirectorBasedOnUserAuthStatus;
