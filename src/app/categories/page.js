f558c3cfe0ef911cb3a168ce1f92315ee2003389
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-02-24 21:12:12 -0600

    wrote api calls for user

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,5 +1,11 @@
+'use client';
+
 import React from 'react';
+import { getUser } from '../../api/userData';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function CategoriesPage() {
+  const { user } = useAuth();
+  getUser(user.uid).then((data) => console.log(data));
   return <div>this is the categories page</div>;
 }
